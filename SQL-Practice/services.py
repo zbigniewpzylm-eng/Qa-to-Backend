4a4c4830d8ade3d9c4e2fc19915e7f6137c584a3
Zbigniew Zylms <zbigniew.p.zylm@gmail.com>
2026-05-12 13:04:04 +0200

    Implement update and delete user with SQLAlchemy session handling

--- SQL-Practice/services.py
+++ SQL-Practice/services.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import IntegrityError
 from fastapi import HTTPException
-from models import Base, User
+from models import User
 
 
 def get_user_by_id(user_id: int, db: Session):
@@ -28,4 +28,42 @@ def create_user(user_data: dict, db: Session):
     db.refresh(user)
     return user
 
+def update_user(user_id: int,user_data_update: dict, db: Session,):
+    # pobieram usera z db
+    db_user = db.get(User, user_id)
+    # zwracam 404 jak nie ma usera o takim ID
+    if db_user is None:
+        raise HTTPException(status_code= 404, detail="User Not found")
+    #definiuje co ma byc zmienione
+    update_data = user_data_update.model_dump(exclude_unset=True)
+    #sprawdzam format danych i updejtuje usera
     
+    if "email" in update_data:
+        db_user.email = update_data["email"].lower()
+    if "name" in update_data:
+        db_user.name = update_data["name"]
+    if "metadata" in update_data:
+        db_user.metadata_ = {
+            **(db_user.metadata_ or {}),
+            **update_data["metadata"]
+    }
+        
+    
+    # próba commita razem z 409 jeśli dupli danych
+    try:
+        db.commit()
+    except IntegrityError:
+        db.rollback()
+        raise HTTPException(409, detail="Email already exists")
+    return db_user
+
+def delete_user_by_id(user_id: int, db: Session):
+    user = db.get(User, user_id)
+
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    else:
+        db.delete(user)
+
+    db.commit()
+         
